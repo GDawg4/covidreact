2a977ae3d74f116a483397d12e6888edd9db219a
Rodrigo Garoz <garoz.r4@gmail.com>
2020-05-24 14:34:34 -0600

    No, really final Qol

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ import './styles.css'
 const Footer = () => (
     <div className='footer'>
         <p><a href={'https://github.com/GDawg4'} target="_blank" className='signature'>Hecho por GDawg4</a>
-            En colaboración con:
+            Con colaboración de:
             Sebastián Gonzáles
             Michael Chan
             Amado García
